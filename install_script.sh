cae3bfb43946e468a3cd8ee0ae667856e5b28de5
Andy Nguyen <Andistar12@users.noreply.github.com>
2020-04-04 16:29:20 -0700

    Update install_script.sh
     - Remove auto elevation prompt at start
     - Merge
     - Add auto tmux plugin install
     - Add git user.email and user.name configuration

--- install_script.sh
+++ install_script.sh
@@ -2,16 +2,7 @@
 
 echo "Setting up computer..."
 
-if [ $EUID != 0 ]; then
-    echo "We need to elevate this script..."
-    sudo "$0" "$@"
-    exit $?
-else
-    echo "This script must be run as a user"
-    exit 1
-fi
-
-echo "Will this be a GUI install? GUI also includes multimedia programs"
+echo "Will this be a GUI install? GUI also includes multimedia programs. This requires snaps"
 select gui_install in "Yes" "No"; do
     case $gui_install in
         Yes ) echo "GUI install selected"; break;;
@@ -22,38 +13,64 @@ done
 echo ""
 echo "Let's first configure your passwd"
 
-sudo passwd
+sudo passwd # This also elevates the script, assuming it's run as user
 
+echo ""
+echo "Let's configure git really quickly"
+
+echo "Enter preferred user.name var:"
+read git_username
+echo "Enter preferred user.email var:"
+read git_email
+
+echo ""
+echo "Thank you. You may now sit back"
+
+echo ""
 echo "Updating package lists..."
 
 sudo apt update
 
-echo "Let's now install vim and tmux to get them out of the way"
-
-sudo apt install -y vim tmux
-echo "Installing vim and tmux configs..."
-# Install vim and tmux configs
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-tmux source-file ~/.tmux.conf
-vim +'PlugInstall --sync' +qa
-
-echo "Vim and tmux installed and configured"
 echo ""
-echo "Let's install more programs"
 echo "Installing terminal programs..."
 
 sudo apt install -y vim tmux git unzip python3-pip pylint htop curl 
 
 case $gui_install in
     Yes ) 
-        echo "Installing GUI commands"
-        sudo apt install -y --simulate pavucontrol default-jre gparted gnome-tweak-tool texlive-full steam chrome-gnome-shell evolution python-opencv openvpn; 
+        echo "";
+        echo "Installing GUI commands";
+        sudo apt install -y --simulate snapd pavucontrol default-jre gparted gnome-tweak-tool texlive-full steam chrome-gnome-shell evolution python-opencv openvpn; 
         #sudo snap install --channel=stable vlc audacity gimp inkscape youtube-dl obs-studio discord handbrake-jz google-play-music-desktop-player mp3gain minecraft-launcher-ot ffmpeg;
         break;;
 esac
 
+echo ""
+echo "Installing vim and tmux configs..."
+
+# Assume fresh-install repo has been cloned, copy config files to home directory
+cp ./.bashrc ~/.bashrc
+cp ./.tmux.conf ~/.tmux.conf
+cp ./.vimrc ~/.vimrc
+# Install vim-plug, tmux plugin maanger, apply both
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+tmux source-file ~/.tmux.conf
+vim +'PlugInstall --sync' +qa
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+echo "Vim and tmux installed and configured"
+
+echo "Setting up git vars"
+git config --global user.name "$git_username"
+git config --global user.email "$git_email"
+
+
+echo ""
 echo "Finally, upgrade system..."
 
 sudo apt upgrade -y
 sudo apt autoremove -y
+
+echo ""
+echo "Installation process finished. A system reboot is recommended if snapd was not preinstalled"
